5f24169460b6620922bd6ad9efeef0645efdac64
Jaime <jaimecr72@gmail.com>
2021-10-07 01:10:41 +0000

    done

---
+++ app.js
@@ -0,0 +1,32 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    let valueInYen = (valueInDollar / 1.2) * 127.9;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen){
+    let valueInPound = (valueInYen/127.9) * 0.8;
+    return valueInPound;
+}
+
+const sum = (a,b) => {
+    return a+b;
+}
+
+console.log(sum(7,3));
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
